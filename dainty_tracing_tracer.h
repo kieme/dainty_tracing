151201d2f3c14cb1b31094ee54b72cfe84fc1030
kieme <frits.germs@gmx.net>
2018-07-18 16:21:16 +0200

    simplify api

--- dainty_tracing_tracer.h
+++ dainty_tracing_tracer.h
@@ -175,49 +175,35 @@ namespace tracer
 {
   using named::p_cstr;
 
+  using t_credit = named::t_uint32;
+
+  enum  t_name_tag_ { };
+  using t_name = named::t_string<t_name_tag_ , 32>;
+
+///////////////////////////////////////////////////////////////////////////////
+
   int_t shared_printf (p_cstr fm, ...) __attribute__((format(printf, 1, 2)));
   int_t shared_vprintf(p_cstr fmt, va_list vars);
 
 ///////////////////////////////////////////////////////////////////////////////
 
-  enum t_option {
-    sourceoption_datetimestamp,     // date/time or nanosec
-    sourceoption_autobindobservers, // bind all observers
-  };
-
-  library::misc::nflags_offset_t t_optiono_offset(t_option);
-  t_option t_optiono_enum(library::misc::nflags_offset_t);
-
-  typedef library::misc::nflags_t<t_option, 8,
-                                  t_optiono_offset,
-                                  t_optiono_enum> sourceoptions_t;
-
-  inline sourceoptions_t operator+(t_option o1, t_option o2) {
-    sourceoptions_t options(o1);
-    options += o2;
-    return options;
-  }
-
-///////////////////////////////////////////////////////////////////////////////
-
-  sourceoptions_t default_options();
-  level_t         default_sourcelevel();
-  credit_t        default_sourcecredit();
+  t_level         default_sourcelevel();
+  t_credit        default_sourcecredit();
 
 ///////////////////////////////////////////////////////////////////////////////
 
   struct t_params {
-    t_params(level_t         level   = default_sourcelevel(),
-                   sourceoptions_t options = default_options(),
-                   credit_t        credit  = default_sourcecredit())
+    t_params(t_level level = default_sourcelevel(),
+             t_bool  bind_to_all_observers,
+             t_bool  ns_format,
+             t_credit        credit  = default_sourcecredit())
       : level_  (level),
         options_(options),
         credit_ (credit)
     { }
 
-    level_t         level_;
-    sourceoptions_t options_;
-    credit_t        credit_;
+    t_level  level_;
+    t_credit credit_;
   };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -226,7 +212,7 @@ namespace tracer
     t_id()                           : offset_(-1),     seq_(0)   { }
     t_id(offset_t offset, seq_t seq) : offset_(offset), seq_(seq) { }
 
-    operator bool_t() const                 { return offset_ != -1; }
+    operator t_bool() const                 { return offset_ != -1; }
 
     t_id release() {
       t_id tmp(*this);
@@ -238,57 +224,34 @@ namespace tracer
     seq_t    seq_;
   };
 
-  bool_t operator==(const t_id&, const t_id&);
-  bool_t operator!=(const t_id&, const t_id&);
+  t_bool operator==(const t_id&, const t_id&);
+  t_bool operator!=(const t_id&, const t_id&);
 
 ///////////////////////////////////////////////////////////////////////////////
 
   // does not require to be here
-  struct t_info_ {
-    t_id           id_;
-    sourcename_t   name_;
-    t_params       params_;
-    usestats_t     usestats_;
+  class t_info_ {
+  public:
+    t_id     id;
+    t_name   name;
+    t_params params;
+    t_stats  stats;
   };
 
 ///////////////////////////////////////////////////////////////////////////////
 
   class t_point {
   public:
-    void_t  post(level_t, const char_t*) const;
-    void_t  post(level_t, const textstring_t&) const;
-    void_t  post(level_t, const compstring_t&, const typestring_t&,
-                          const codestring_t&, const textstring_t&) const;
+    void_t post(t_level, t_cptr) const;
+    void_t post(t_level, const textstring_t&) const;
 
-    void_t  guaranteed_post(level_t, const char_t*) const;
-    void_t  guaranteed_post(level_t, const textstring_t&) const;
-    void_t  guaranteed_post(level_t, const compstring_t&,
-                                     const typestring_t&,
-                                     const codestring_t&,
-                                     const textstring_t&) const;
+    void_t certain_post(t_level, t_cptr) const;
+    void_t certain_post(t_level, const textstring_t&) const;
 
-    bool_t       set_name(const sourcename_t&);
-    sourcename_t get_name() const;
+    t_name  get_name () const;
+    t_level get_level() const;
 
-    level_t get_level() const;
-    void_t  set_level(level_t);
-    bool_t  match    (level_t) const;
-
-    bool_t          add_options(sourceoptions_t);
-    void_t          del_options(sourceoptions_t);
-    sourceoptions_t get_options() const;
-
-    void_t   set_credit(credit_t);
-    credit_t get_credit() const;
-
-    bool_t bind(const observerid_t&);
-    bool_t bind(const observername_t&);
-
-    void_t unbind();
-    bool_t unbind(const observerid_t&);
-    bool_t unbind(const observername_t&);
-
-    operator bool_t() const                                { return id_; }
+    operator t_validity() const { return id_; }
 
   private:
     t_point(const t_id& id) : id_(id)                                { }
@@ -317,18 +280,8 @@ namespace tracer
     inline       t_point* operator->()        { return &point_; }
     inline const t_point* operator->() const  { return &point_; }
 
-    inline       t_point& operator* ()        { return  point_; }
-    inline const t_point& operator* () const  { return  point_; }
-
-    inline       t_point*  ptr ()             { return &point_; }
-    inline const t_point*  ptr () const       { return &point_; }
-    inline const t_point* cptr () const       { return &point_; }
-
-    inline       t_point&  ref ()             { return point_; }
-    inline const t_point&  ref () const       { return point_; }
-    inline const t_point& cref () const       { return point_; }
-
-    inline t_void release   ()                { return *this;   }
+    inline       t_point& operator*()         { return point_; }
+    inline const t_point& operator*() const   { return point_; }
 
     inline t_point make_point(t_user);
 
@@ -340,9 +293,6 @@ namespace tracer
   };
 
 ////////////////////////////////////////////////////////////////////////////////
-
-  inline       t_point& ref(      t_point* point)  { return *point;}
-  inline const t_point& ref(const t_point* point)  { return *point;}
 
   inline       t_point& ref(      t_point& point)  { return point; }
   inline const t_point& ref(const t_point& point)  { return point; }
